7a8e91c83bf4d967dc23ca6b61b76b8a5aedbeb9
stive-junior <nigninguioadeline@gmail.com>
2026-02-12 09:33:28 +0100

    v2 du chat: input d'envoi

--- backend/server.js
+++ backend/server.js
@@ -707,9 +707,11 @@ const jwtAuth = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader?.startsWith('Bearer ')) {
         return res.status(401).json({ success: false, error: 'Token requis' });
+    
     }
 
     const token = authHeader.split(' ')[1];
+
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
         req.user = payload;
